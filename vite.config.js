c0995c3d6a80902315b873756d8598fa3a40c84e
Sasikanlkc <sasikan.lkc@gmail.com>
2026-02-16 21:38:02 +0700

    update config auto import

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Pages from 'vite-plugin-pages'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/rass/',
   plugins: [
     vue(),
+    Pages({
+      dirs: 'src/pages',
+      extensions: ['vue'],
+      routeStyle: 'nuxt'
+    }),
     vueDevTools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
