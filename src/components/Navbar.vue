00166d0bf09c454eff9a9c016eb04324e5d1e44d
Sasikanlkc <sasikan.lkc@gmail.com>
2026-02-24 22:57:35 +0700

    change navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,125 +1,168 @@
 <template>
-  <div class="navbar bg-base-100 px-14 py-1 shadow-sm sticky top-0 z-40">
+  <div class="navbar bg-base-100 px-10 py-1 shadow-sm sticky top-0 z-40">
     <!-- logo -->
-    <div class="start mr-5">
-      <router-link :to="logoRoute">
-        <img
-          class="flex-auto max-w-14 w-24"
-          src="https://scholar.it.kmitl.ac.th/images/it-kmitl.png"
-          alt="IT KMITL"
-        />
-      </router-link>
-    </div>
-
-    <!-- menu -->
-    <div class="flex-auto w-full flex-row">
-      <div class="menu menu-horizontal px-5">
-        <div v-if="user" class="flex flex-row">
-          <div v-if="!isAdmin && !isProfessor" class="justify-center">
-            <div tabindex="0" role="button" class="btn btn-ghost">
-              <router-link to="/Officer" class="font-normal"
-                >ตรวจสอบเอกสาร</router-link
-              >
-            </div>
-            <div class="dropdown">
-              <div tabindex="0" role="button" class="btn btn-ghost">
-                <p class="font-normal">ยื่นขอรับการสนับสนุน &nbsp; &#9660;</p>
-              </div>
-              <ul
-                tabindex="0"
-                class="dropdown-content menu bg-base-100 rounded-t-none p-2 shadow"
-              >
+    <div class="navbar-start grow gap-5">
+      <div class="dropdown">
+        <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
+          </svg>
+        </div>
+        <ul
+          tabindex="-1"
+          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+        >
+          <li
+            v-if="hasRole(['hr', 'research', 'finance', 'associate', 'dean'])"
+          >
+            <router-link to="/Officer">ตรวจสอบเอกสาร</router-link>
+          </li>
+          <li
+            v-if="
+              hasRole([
+                'professor',
+                'hr',
+                'research',
+                'finance',
+                'associate',
+                'dean',
+              ])
+            "
+          >
+            <details>
+              <summary>ยื่นการขอรับการสนับสนุน</summary>
+              <ul class="p-2 bg-base-100 w-fit z-1">
                 <li><router-link to="/homePage">ยื่นเอกสาร</router-link></li>
                 <li>
-                  <router-link to="/myHistory">สถานะ และประวัติเอกสาร</router-link>
+                  <router-link to="/myHistory"
+                    >สถานะ และประวัติการยื่นเอกสาร</router-link
+                  >
                 </li>
               </ul>
-            </div>
-          </div>
-
-          <div v-if="isProfessor" class="justify-center">
-            <div tabindex="0" role="button" class="btn btn-ghost">
-              <router-link to="/homePage" class="font-normal">ยื่นเอกสาร</router-link>
-            </div>
-            <div tabindex="0" role="button" class="btn btn-ghost">
-              <router-link to="/myHistory" class="font-normal">สถานะ และประวัติเอกสาร</router-link>
-            </div>
-          </div>
-
-          <div v-if="isHRorResearch" class="justify-center">
-            <div tabindex="0" role="button" class="btn btn-ghost">
-              <router-link to="/eOffice">เอกสารรออนุมัติ</router-link>
-            </div>
-          </div>
-
-          <div v-if="isApprover" class="justify-center">
-            <div tabindex="0" role="button" class="btn btn-ghost">
-              <router-link to="/allHistory" class="font-normal"
-                >สถานะ และประวัติเอกสาร</router-link
-              >
-            </div>
-          </div>
-
-          <div v-if="isFinance" class="justify-center">
-            <div tabindex="0" role="button" class="btn btn-ghost">
-              <router-link to="/allWithdrawMoney" class="font-normal"
-                >ขออนุมัติเบิกเงินรายได้</router-link
-              >
-            </div>
-          </div>
-
-          <div v-if="isAdmin" class="justify-center">
-            <div tabindex="0" role="button" class="btn btn-ghost">
-              <router-link to="/changePage" class="font-normal"
-                >เปลี่ยนเงื่อนไขการพิจารณา</router-link
-              >
-            </div>
-          </div>
-        </div>
-
-        <div class="justify-center">
-          <div tabindex="0" role="button" class="btn btn-ghost">
-            <router-link to="/" class="font-normal">สรุปผลแบบสถิติ</router-link>
-          </div>
-        </div>
+            </details>
+          </li>
+          <li v-if="hasRole(['hr', 'research'])">
+            <router-link to="/eOffice">เอกสารรออนุมัติ</router-link>
+          </li>
+          <li
+            v-if="hasRole(['hr', 'research', 'finance', 'associate', 'dean'])"
+          >
+            <router-link to="/allHistory">สถานะ และประวัติเอกสาร</router-link>
+          </li>
+          <li v-if="hasRole(['finance'])">
+            <router-link to="/allWithdrawMoney"
+              >ขออนุมัติเบิกเงินรายได้</router-link
+            >
+          </li>
+          <li v-if="hasRole(['admin'])">
+            <router-link to="/changePage"
+              >เปลี่ยนเงื่อนไขการพิจารณา</router-link
+            >
+          </li>
+          <li><router-link to="/">สรุปผลแบบสถิติ</router-link></li>
+        </ul>
       </div>
-    </div>
 
-    <div v-if="!userStore.user">
-      <router-link
-        to="/login"
-        class="btn w-full bg-[#4285F4] hover:bg-[#4285F4] text-white ml-10"
-      >
-        เข้าสู่ระบบ
+      <router-link :to="logoRoute">
+        <img
+          class="max-w-14"
+          src="https://scholar.it.kmitl.ac.th/images/it-kmitl.png"
+          alt="LOGO IT KMITL"
+        />
       </router-link>
+
+      <div class="hidden lg:flex">
+        <ul class="menu menu-horizontal px-1">
+          <li
+            v-if="hasRole(['hr', 'research', 'finance', 'associate', 'dean'])"
+          >
+            <router-link to="/Officer">ตรวจสอบเอกสาร</router-link>
+          </li>
+          <li
+            v-if="
+              hasRole([
+                'professor',
+                'hr',
+                'research',
+                'finance',
+                'associate',
+                'dean',
+              ])
+            "
+          >
+            <details>
+              <summary>ยื่นการขอรับการสนับสนุน</summary>
+              <ul class="p-2 bg-base-100 w-fit z-1">
+                <li><router-link to="/homePage">ยื่นเอกสาร</router-link></li>
+                <li>
+                  <router-link to="/myHistory"
+                    >สถานะ และประวัติการยื่นเอกสาร</router-link
+                  >
+                </li>
+              </ul>
+            </details>
+          </li>
+          <li v-if="hasRole(['hr', 'research'])">
+            <router-link to="/eOffice">เอกสารรออนุมัติ</router-link>
+          </li>
+          <li
+            v-if="hasRole(['hr', 'research', 'finance', 'associate', 'dean'])"
+          >
+            <router-link to="/allHistory">สถานะ และประวัติเอกสาร</router-link>
+          </li>
+          <li v-if="hasRole(['finance'])">
+            <router-link to="/allWithdrawMoney"
+              >ขออนุมัติเบิกเงินรายได้</router-link
+            >
+          </li>
+          <li v-if="hasRole(['admin'])">
+            <router-link to="/changePage"
+              >เปลี่ยนเงื่อนไขการพิจารณา</router-link
+            >
+          </li>
+          <li><router-link to="/">สรุปผลแบบสถิติ</router-link></li>
+        </ul>
+      </div>
     </div>
 
-    <!-- user menu -->
-    <div class="flex flex-auto justify-end w-full" v-if="userStore.user">
-      <div class="menu menu-horizontal px-5">
-        <div class="dropdown">
-          <div
-            tabindex="0"
-            role="button"
-            class="btn bg-[#4285F4] hover:bg-[#4285F4] py-[13px] text-white min-w-32"
-          >
-            <p class="font-normal">
-              {{ userStore.user.user_nameth }} &nbsp; &#9660;
-            </p>
-          </div>
-          <ul
-            tabindex="0"
-            class="dropdown-content menu bg-base-100 rounded-t-none p-2 shadow"
-          >
-            <li v-if="userStore.user?.user_role != 'admin'">
-              <router-link to="/profile">ข้อมูลส่วนตัว</router-link>
-            </li>
-            <li @click="logout">
-              <router-link to="/">ออกจากระบบ</router-link>
-            </li>
-          </ul>
-        </div>
+    <div class="navbar-end flex-1">
+      <div v-if="!user">
+        <router-link
+          to="/login"
+          class="btn w-fit bg-[#4285F4] hover:bg-[#4285F4] text-white"
+        >
+          เข้าสู่ระบบ
+        </router-link>
       </div>
+
+      <ul class="menu menu-horizontal px-1" v-if="user">
+        <li>
+          <details>
+            <summary
+              class="btn w-fit bg-[#4285F4] hover:bg-[#4285F4] text-white content-center"
+            >
+              {{ userStore.user.user_nameth }}
+            </summary>
+            <ul class="p-2 bg-base-100 w-fit z-1">
+              <li><router-link to="/profile">ข้อมูลส่วนตัว</router-link></li>
+              <li @click="logout">
+                <router-link to="/">ออกจากระบบ</router-link>
+              </li>
+            </ul>
+          </details>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -144,17 +187,12 @@ const logoRoute = computed(() => {
   return "/";
 });
 
-const isAdmin = computed(() => user.value?.user_role === "admin");
-const isProfessor = computed(() => user.value?.user_role === "professor");
-const isFinance = computed(() => user.value?.user_role === "finance");
-const isHRorResearch = computed(() =>
-  ["hr", "research"].includes(user.value?.user_role)
-);
-const isApprover = computed(() =>
-  ["hr", "research", "finance", "associate", "dean"].includes(
-    user.value?.user_role
-  )
-);
+const role = computed(() => user.value?.user_role);
+
+const hasRole = (roles) => {
+  if (!role.value) return false;
+  return roles.includes(role.value);
+};
 
 const logout = async () => {
   await userStore.logout();
